946a78f98436588194360abdb5a82cf03a4cc0d1
Miyoshi-Ryota <m1yosh1.ry0t4@gmail.com>
2020-08-25 06:51:24 +0900

    サンプリング数を変更可能にした

--- R/calculateAllElaOnAny.R
+++ R/calculateAllElaOnAny.R
@@ -3,13 +3,14 @@
 #' @export
 
 
-lhsSampling = function(lowerbound, upperbound, dimension) {
+lhsSampling = function(lowerbound, upperbound, dimension, numberOfSample) {
+    # numberOfSampleの目安は dimension * 50
     lowerbound = -5
     upperbound = 5
     ctrl = list(init_sample.type = "lhs",
       init_sample.lower = lowerbound,
       init_sample.upper = upperbound)
-    return(createInitialSample(n.obs = 50 * dimension, dim = dimension, control = ctrl))
+    return(createInitialSample(n.obs = numberOfSample, dim = dimension, control = ctrl))
 }
 
 calculateAllFeatures = function(x, y) {
